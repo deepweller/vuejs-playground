210eb242d2edd998c8b18073203942d2a746097e
박진영(Jinyeong Park)/RM개발팀/11ST <pjy0902@sk.com>
2020-01-14 08:43:25 +0900

    single component

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // javascript, 인스턴스 옵션
+  methods: {
+    addNum: function(num) {
+      return num++;
+    }
+  }
+};
+</script>
+
+<style>
+/* css */
+</style>
