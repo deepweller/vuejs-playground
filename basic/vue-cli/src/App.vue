8d27d42ba5bb2075ec750474607493bc882cccaf
박진영(Jinyeong Park)/RM개발팀/11ST <pjy0902@sk.com>
2020-01-14 13:38:16 +0900

    single component sample

--- basic/vue-cli/src/App.vue
+++ basic/vue-cli/src/App.vue
@@ -2,10 +2,16 @@
   <!-- div 가 가장 상위 레벨이여야함 -->
   <div>
     {{ str }}
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
+//상대경로
+import AppHeader from './components/AppHeader.vue'
+
 // new Vue({
 //   data : {
 //     str: 'hi'
@@ -15,7 +21,16 @@ export default {
   //객체 리터럴이 아닌 function 으로 선언해야함
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header'
+    }
+  },
+  //객체 리터럴
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi!!";
     }
   }
 }
