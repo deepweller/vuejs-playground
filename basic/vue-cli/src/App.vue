ce6dd0c5adc96f8bb68ee682d008f7a3a1ef1b63
박진영(Jinyeong Park)/RM개발팀/11ST <pjy0902@sk.com>
2020-01-14 13:13:40 +0900

    edit app.vue

--- basic/vue-cli/src/App.vue
+++ basic/vue-cli/src/App.vue
@@ -1,37 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    
-    <!-- 컴포넌트 등록방법 -->
-    <hello-world></hello-world> <!-- 이 표기법만 vscode 에서 해당 컴포넌트 vue 파일로 바로가기 지원 -->
-    <HelloWorld></HelloWorld>
-    <HelloWorld/>
+  <!-- div 가 가장 상위 레벨이여야함 -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-// new Vue() 와 동일
+// new Vue({
+//   data : {
+//     str: 'hi'
+//   }
+// })
 export default {
-  //인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'app',
-  components: {
-    HelloWorld,
-    //위와 동일한 의미
-    'hello-world': HelloWorld
+  //객체 리터럴이 아닌 function 으로 선언해야함
+  data: function() {
+    return {
+      str: 'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
