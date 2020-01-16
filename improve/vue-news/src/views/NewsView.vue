58054e35ba6d72a1e475b00ab00a0979009df178
deepweller <pjy0902@sk.com>
2020-01-17 08:23:15 +0900

    call api

--- improve/vue-news/src/views/NewsView.vue
+++ improve/vue-news/src/views/NewsView.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-      news
+    <div v-for="user in users">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    var vm = this;
+    // => syntax 사용 전
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then(function(response) {
+        vm.users = response.data;
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+        
+      });
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
