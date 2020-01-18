8ad60aec55f7e74f6907c9636c34ea8a852cfaf6
jinyeong <jinyp91@gmail.com>
2020-01-18 17:39:59 +0900

    add api data in page

--- improve/vue-news/src/views/AskView.vue
+++ improve/vue-news/src/views/AskView.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <!-- 속성에 접근횟수가 많아짐 -->
-    <div v-for="ask in asks">{{ ask.title }}</div>
+    <!-- this.$store.state.jobs : 속성에 접근횟수가 많아짐 >> mapGetters 사용 -->
+    <!-- <div v-for="ask in asks">{{ ask.title }}</div> -->
+    <p v-for="ask in asks">
+      <a v-bind:href="ask.url">
+        {{ ask.title }}
+      </a>
+      <small>{{ ask.time_ago }} by {{ ask.user }}</small>
+    </p>
   </div>
 </template>
 
