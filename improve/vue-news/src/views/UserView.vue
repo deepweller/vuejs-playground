ed791b775665d4ddb735126b4c962c1647ef5eb3
jinyeong <jinyp91@gmail.com>
2020-01-19 10:01:08 +0900

    use computed

--- improve/vue-news/src/views/UserView.vue
+++ improve/vue-news/src/views/UserView.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <p>name: {{ this.$store.state.user.id }}</p>
-    <p>karma: {{ this.$store.state.user.karma }}</p>
-    <p>created: {{ this.$store.state.user.created }}</p>
+    <p>name: {{ userInfo.id }}</p>
+    <p>karma: {{ userInfo.karma }}</p>
+    <p>created: {{ userInfo.created }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    } 
+  },
   created() {
     const userName = this.$route.params.id;
     this.$store.dispatch('FETCH_USER', userName);
