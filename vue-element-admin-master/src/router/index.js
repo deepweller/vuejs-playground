8cc10dc87c6466258f80e198424aedaea9fd4705
jinyeong <jinyp91@gmail.com>
2020-01-31 14:21:59 +0900

    add sample
    - add side bar menu
    - add custom page
    - set ui-element locale
    - router history mode

--- vue-element-admin-master/src/router/index.js
+++ vue-element-admin-master/src/router/index.js
@@ -11,6 +11,7 @@ import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
+import customRouter from './modules/custom'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -83,6 +84,7 @@ export const constantRoutes = [
       }
     ]
   },
+  customRouter,
   {
     path: '/documentation',
     component: Layout,
@@ -388,7 +390,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
---
+++ vue-element-admin-master/src/router/modules/custom.js
@@ -0,0 +1,33 @@
+import Layout from '@/layout'
+
+const customRouter = {
+  path: '/custom',
+  component: Layout,
+  redirect: '/custom/complex-table',
+  name: 'Custom',
+  meta: {
+    title: 'Custom',
+    icon: 'people'
+  },
+  children: [
+    {
+      path: 'sample-form',
+      component: () => import('@/views/custom/sample-form'),
+      name: 'SampleForm',
+      meta: { title: 'Sample Form' }
+    },
+    {
+      path: 'complex-table',
+      component: () => import('@/views/table/complex-table'),
+      name: 'ComplexTable',
+      meta: { title: 'Complex Table' }
+    },
+    {
+      path: 'keyboard',
+      component: () => import('@/views/charts/keyboard'),
+      name: 'KeyboardChart',
+      meta: { title: 'Keyboard Chart', noCache: true }
+    }
+  ]
+}
+export default customRouter
